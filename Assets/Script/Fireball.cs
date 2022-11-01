4f1c175126ee323c5a77e3b05a8b684fd8f4c363
Armando Interaminense <ar.interaminense@gmail.com>
2022-11-01 13:37:00 -0400

    enemy don't shoot if player is at a higher place

--- Assets/Script/Fireball.cs
+++ Assets/Script/Fireball.cs
@@ -5,22 +5,29 @@ public class Fireball : MonoBehaviour
 {
     public float speed = 1f;
     public Player player;
+    public Enemy enemy;
     public Vector3 target;
 
     private void Start()
     {
         player = FindObjectOfType<Player>();
+        enemy = FindObjectOfType<Enemy>();
         target = player.transform.position;
     }
     private void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target, speed*Time.deltaTime); 
-        //transform.position += Vector3.left*speed*Time.deltaTime; // move the ball
+        float dir = enemy.transform.position.y - target.y;
+        if (dir > -1f && dir < 1f) //Enemy just shoot if player is not in higher place
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        }
+
         StartCoroutine(DestroyFireball()); // destroy fireball after 4 seconds
     }
     IEnumerator DestroyFireball()
     {
-        yield return new WaitForSeconds(4f);
+        yield return new WaitForSeconds(2f);
         Destroy(this.gameObject);
     }
 
